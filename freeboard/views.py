6acbfe612465064a65fc95bdc17e6bb3ce322a85
YoonYoon <jin71002479@gmail.com>
2022-01-26 10:14:14 +0900

    220126_1013

--- freeboard/views.py
+++ freeboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.shortcuts import redirect
+from userapp.models import User
 
 # Create your views here.
