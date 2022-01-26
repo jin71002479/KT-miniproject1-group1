0db7e3d926a5f35299575112c894f99f12b28a00
devY <ocean1997@naver.com>
2022-01-26 17:06:49 +0900

    검색구현

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import rank_data
 from django.db import connection
+from sympy import content
 from userapp.models import User
+from board.models import Question
 # Create your views here.
 def index(request):
     rank_list = User.objects.all().order_by('-score')
@@ -19,3 +20,17 @@ def index2(request):
         'main/index2.html',
         {'rank_list': rank_list}
     )
+
+def search(request):
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        sel=request.POST.get('sel')
+        result=Question.objects.filter(subject__contains=search)
+        if sel=='Q&A':
+            return render(
+                request,
+                'main/show.html',
+                {'data':result}
+            )
+
+    
