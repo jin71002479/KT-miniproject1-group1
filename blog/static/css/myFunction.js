f90cc5db5d7afe0ebf87af2d0bee451a65c34642
smalleyescoding <smalleyescoding@gmail.com>
2022-01-26 14:08:30 +0900

    블로그 완성

--- blog/static/css/myFunction.js
+++ blog/static/css/myFunction.js
@@ -15,10 +15,25 @@ function myFunction() {
 function myFunction10() {
 var checkBox = document.getElementById("tag_10");
 var text10 = document.getElementById("text10");
+var text11 = document.getElementById("text11");
+var text12 = document.getElementById("text12");
+var text13 = document.getElementById("text13");
+var text14 = document.getElementById("text14");
+var text15 = document.getElementById("text15");
 if (checkBox.checked == true){
   text10.style.display = "block";
+  text11.style.display = "block";
+  text12.style.display = "block";
+  text13.style.display = "block";
+  text14.style.display = "block";
+  text15.style.display = "block";
 } else {
     text10.style.display = "none";
+    text11.style.display = "none";
+    text12.style.display = "none";
+    text13.style.display = "none";
+    text14.style.display = "none";
+    text15.style.display = "none";
 }
 }
 
@@ -33,3 +48,17 @@ text20.style.display = "block";
 }
 }
 
+
+function myFunction30() {
+  var checkBox = document.getElementById("tag_30");
+  var text30 = document.getElementById("text30");
+  var text31 = document.getElementById("text31");
+  if (checkBox.checked == true){
+  text30.style.display = "block";
+  text31.style.display = "block";
+  } else {
+    text30.style.display = "none";
+    text31.style.display = "none";
+  }
+  }
+  
