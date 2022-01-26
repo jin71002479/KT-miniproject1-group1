2517a7d2718e4166c9a3e0abeba0f96e50569c71
YoonYoon <jin71002479@gmail.com>
2022-01-26 10:28:45 +0900

    220126_1028

--- freeboard/views.py
+++ freeboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+from .models import Freewrite
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
 from userapp.models import User
 
-# Create your views here.
+def index(request):
+    question_list = Freewrite.objects.order_by('-free_pub_date')
+    context = {'question_list': question_list}
+    return render(request, 'board/question_list.html', context)
